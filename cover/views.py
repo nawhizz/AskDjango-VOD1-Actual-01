f39111108b3e0ab5a6fbd730d0fbf6c6f6233798
You Youngbai <nawhizz@gmail.com>
2019-03-23 18:15:14 +0900

    image_generator 함수 추가

--- cover/views.py
+++ cover/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.http import HttpResponse
+from PIL import Image
+from .forms import CoverForm
 
 
 def index(request):
-    return render(request, 'cover/index.html')
+    if request.method == 'POST':
+        form = CoverForm(request.POST)
+        if form.is_valid():
+            form.cleaned_data   # 이미지를 생성할 데이터는 여기에 있고!!!
+            return redirect('cover:index')
+    else:
+        form = CoverForm()
+    return render(request, 'cover/index.html', {
+        'form': form,
+    })
+
+
+# 위 데이터를 받아서, 이미지는 여기에서 그리겠습니다~ ;)
+def image_generator(request):
+    im = Image.new('RGB', (256, 256), 'yellow')
+    reponse = HttpResponse(content_type='image/jpeg')    # file-like
+    im.save(reponse, format='JPEG')
+    return reponse
